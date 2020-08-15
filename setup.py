91f8e96ae083a233dfed259383b19aa041f8bb48
Determinant <tederminant@gmail.com>
2020-08-15 12:48:14 -0400

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='keytree.py',
+      version='0.2',
+      description='Derive BIP32 key pairs from BIP39 mnemonic',
+      url='http://github.com/Determinant/keytree.py',
+      author='Ted Yin',
+      author_email='tederminant@gmail.com',
+      license='MIT',
+      scripts=['keytree.py'],
+      install_requires=['ecdsa', 'base58', 'pysha3'])
